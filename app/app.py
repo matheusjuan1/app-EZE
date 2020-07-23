644c6f87084f52eb7b3eb4125246c92594fdb33d
Aluno <gabriel.pinheiro@bandtec.com.br>
2020-07-23 18:15:31 -0300

    minhas alterações promoter

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 import os
 
-
+import sqlite3
 from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
 # from flask_session import Session
 from tempfile import mkdtemp
@@ -31,12 +31,12 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 # Session(app)
 
-<<<<<<< HEAD
-=======
+# <<<<<<< HEAD
+# =======
 # Configure CS50 Library to use SQLite database
 # db = SQL("sqlite:///eze.db")
 
->>>>>>> 8f87bdb325ee2afb62948ebc85f0762e4a483aa0
+# >>>>>>> 8f87bdb325ee2afb62948ebc85f0762e4a483aa0
 # Make sure API key is set
 # if not os.environ.get("API_KEY"):
 #     raise RuntimeError("API_KEY not set")
@@ -89,6 +89,8 @@ def promoterLogado():
         db.row_factory = sqlite3.Row
         eze = db.cursor()
         eze.execute("SELECT * FROM promoters WHERE nomePromoter = 'Vitória Gama'")
+        eze.execute("ALTER TABLE lista ADD fk_promoter integer")
+        eze.execute("ALTER TABLE promoters DROP COLUMN fk_promoter")
         linhas = eze.fetchall()
         return render_template("promoter.html", banco = linhas)
 
